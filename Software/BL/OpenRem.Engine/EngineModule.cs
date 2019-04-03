95d445bf05a96d8e0ce632dd61cd2b8078fa7471
Paweł Sołtysiak <pawel@soltysiak.it>
2019-04-03 10:48:24 +0200

    Added AutoFac configuration

---
+++ Software/BL/OpenRem.Engine/EngineModule.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Autofac;
+using Module = Autofac.Module;
+
+namespace OpenRem.Engine
+{
+    public class EngineModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var dataAccess = Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(dataAccess)
+                .AsImplementedInterfaces();
+        }
+    }
+}
