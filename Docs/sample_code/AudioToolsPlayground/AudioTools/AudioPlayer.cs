8c642752cb847d9195f8b80abc0c33ec1cfdbc81
pheu <lisik.rafal@gmail.com>
2019-04-16 21:49:08 +0200

    Misc/audio tools (#19)
    * added audio tools solution
    * renamed solution and project

---
+++ Docs/sample_code/AudioToolsPlayground/AudioTools/AudioPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Threading;
+using AudioTools.Interface;
+using NAudio.Wave;
+
+namespace AudioTools
+{
+    public class AudioPlayer : IAudioPlayer
+    {
+        public void PlaySound(ISound sound)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // TODO: Make a separate thread
+        public void PlaySound(Stream stream, int samplingRate, BitDepth bitDepth, Channels channels)
+        {
+            var rawWaveStream = new RawSourceWaveStream(stream, new WaveFormat(samplingRate, (int)bitDepth, (int)channels));
+            var waveOutEvent = new WaveOutEvent();
+            waveOutEvent.Init(rawWaveStream);
+            waveOutEvent.Play();
+            while (waveOutEvent.PlaybackState == PlaybackState.Playing)
+            {
+                Thread.Sleep(500);
+            }
+            waveOutEvent.Dispose();
+        }
+        
+        public void PlaySound(byte[] data, int samplingRate, BitDepth bitDepth, Channels channels)
+        {
+            PlaySound(new MemoryStream(data), samplingRate, bitDepth, channels);
+        }
+    }
+}
---
+++ Docs/sample_code/AudioToolsPlayground/AudioToolsPlayground/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using AudioTools;
+using AudioTools.Interface;
+using AudioToolsPlayground.Commands;
+
+namespace AudioToolsPlayground
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private IAudioPlayer _audioPlayer;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainWindowViewModel()
+        {
+            _audioPlayer = new AudioPlayer();
+        }
+
+        public ICommand PlaySoundCommand
+        {
+            get { return new DelegateCommand(PlaySound); }
+        }
+
+        private void PlaySound()
+        {
+            var sampleRate = 16000;
+            var frequency = 500;
+            var amplitude = 0.2;
+            var seconds = 5;
+
+            var raw = new byte[sampleRate * seconds * 2];
+
+            var multiple = 2.0*frequency/sampleRate;
+            for (int n = 0; n < sampleRate * seconds; n++)
+            {
+                var sampleSaw = ((n*multiple)%2) - 1;
+                var sampleValue = sampleSaw > 0 ? amplitude : -amplitude;
+                var sample = (short)(sampleValue * Int16.MaxValue);
+                var bytes = BitConverter.GetBytes(sample);
+                raw[n*2] = bytes[0];
+                raw[n*2 + 1] = bytes[1];
+            }
+            
+            _audioPlayer.PlaySound(raw, sampleRate, BitDepth.Of16, Channels.Mono);
+        }
+    }
+}
